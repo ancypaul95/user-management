ed1981489d5a6e90d6b8b366fb56b2e76e802943
Ancy Paul <ancypaul9559@gmail.com>
2023-07-22 00:46:30 +0530

    Initial Commit

---
+++ node-app/server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+require("dotenv").config();
+
+// Connect Mongodb Database
+const db = process.env.MONGODB_URI;
+
+mongoose.connect(db, { useNewUrlParser: true }).then(
+  () => {
+    console.log("Database is connected");
+  },
+  (err) => {
+    console.log("There is problem while connecting database " + err);
+  }
+);
+
+// Conver incoming data to JSON format
+app.use(bodyParser.json());
+
+// Enabled CORS
+app.use(cors());
+
+const port = process.env.PORT || 8082;
+app.listen(port, () => console.log(`server listening on ${port}`));
+module.exports = app;
